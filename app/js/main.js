3d1b9423bb745c501aef218934ebd8dc1cbcfa20
Iryna Sokolovska <sokirina777@gmail.com>
2024-06-01 13:51:53 +0300

    fixed first part

--- app/js/main.js
+++ app/js/main.js
@@ -5,4 +5,14 @@ $(function () {
   });
 });
 
+const headerEl = document.querySelector(".header__inner");
+window.addEventListener("scroll", function () {
+  const scrollPos = this.window.scrollY;
+  if (scrollPos > 100) {
+    headerEl.classList.add("header__inner--fixed");
+  } else {
+    headerEl.classList.remove("header__inner--fixed");
+  }
+});
+
 var mixer = mixitup(".popular__content");
